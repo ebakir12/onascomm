aa0a7221849c5646c4dd2eb0274394a79c90a991
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-10-01 01:36:50 -0500

    Add files via upload

---
+++ cgate_self_signed_cert/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'SSL Certificate Generator',
+    'category': 'Integration',
+    'sequence': 55,
+    'summary': 'Manage SSL Certificates',
+    'website': '',
+    'version': '1.0',
+    'description': "A Way to link and manage the SSL certificates needed for the local Server and to store an external server certificate.",
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+}
