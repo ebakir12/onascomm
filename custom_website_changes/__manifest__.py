45ad1045bd8330b8a8773a4ae45939023631999b
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-11-16 00:45:36 -0600

    Add files via upload

---
+++ custom_website_changes/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'eCommerce Custom Website Changes',
+    'category': 'Website',
+    'version': '1.0',
+    'website': '',
+    'description': """
+Odoo E-Commerce
+==================
+Custom Changes to the website:
+-Add custom field to Template to display in Sales Ribbon 
+        """,
+    'depends': ['website_sale'],
+    'data': [
+        'views/product_views.xml'
+    ],
+    'installable': True,
+}
