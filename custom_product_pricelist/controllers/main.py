17ffffac9ea19fc071bea066f1e5961f9ec8ddb4
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-11-18 17:22:00 -0600

    Add files via upload

--- custom_product_pricelist/controllers/main.py
+++ custom_product_pricelist/controllers/main.py
@@ -1,32 +1,36 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import babel
 import json
 import logging
 from werkzeug.exceptions import Forbidden, NotFound
-
-from odoo import http, tools, _
+from datetime import datetime, timedelta, time
+from odoo import fields, http, tools, _
 from odoo.http import request
+from odoo.addons.website_sale_options.controllers.main import WebsiteSaleOptions
 
+_logger = logging.getLogger(__name__)
 
-class WebsitePricelist(http.Controller):
+"""
+class WebsiteDealerInfo(http.Controller):
     
-    """
-	def get_dealer_info_ids(self):
-        """ list of active selectable dealer info
+    def get_dealer_info_ids(self):
+        "" list of active selectable dealer info
 
         :return: list of dealer info
            ()
-        """
+        ""
         # dealer info
-        dealer_info = http.request.env['dealer_info.dealerinfo']
-        return  dealer_info.search(['is_active', '=', 'true'])
-	"""
+        dealer_info = http.request.env['dealer.info']
+        dealers = dealer_info.search(['is_active', '=', 'true'])
+        return  dealers
+
 
         
     # ------------------------------------------------------
     # TELUS Redirect
     # ------------------------------------------------------
-    """
+    ""
     def telus_redirection(self, order):
         # must have a draft sales order with lines at this point, otherwise reset
         if not order or order.state != 'draft':
@@ -41,4 +45,47 @@ class WebsitePricelist(http.Controller):
         tx = request.env.context.get('website_sale_transaction')
         if tx and tx.state != 'draft':
             return request.redirect('/shop/payment/confirmation/%s' % order.id)
-    """
+    ""
+    @http.route('/telus/<model("dealer.info"):dealer>', auth='user')
+    def telus_redirect(self, dealer):
+        if self.env.user != SUPERUSER_ID:
+            self.env.user.dealer_info_id = dealer
+        websettings = self.pool.get('sale.config.settings').browse(cr,uid,ids)[0]
+        response = redirect("https://evs.telus.com/evs?channel_id=" + websettings.channel_id + "&amp;campaign_id=" + websettings.campaign_id + "&amp;RCID=" + dealer.rcid)
+        return response
+"""
+	
+class InheritWebsiteSaleOptions(WebsiteSaleOptions):	
+
+    @http.route(['/shop/cart/update_option'], type='http', auth="public", methods=['POST'], website=True, multilang=False)
+    def cart_options_update_json(self, product_id, add_qty=1, set_qty=0, goto_shop=None, lang=None, **kw):
+        res = super(InheritWebsiteSaleOptions, self).cart_options_update_json(
+            product_id=product_id, 
+            add_qty=add_qty, 
+            set_qty=set_qty, 
+            lang=lang,
+            **kw
+	)
+        _logger.info('*** Inside the custom update option')	
+        
+        product = request.env['product.product'].browse(int(product_id))
+
+        attributes = self._filter_attributes(**kw)
+        
+        order = request.website.sale_get_order()
+	
+        # add a taxable line
+        if product.taxable_product_id:
+            tax_lines = order.sudo().order_line.filtered(lambda x: x.product_id.id == int(product.taxable_product_id.id))
+            tax_linked_ids = [x.linked_line_id for x in tax_lines]
+            prod_lines = order.sudo().order_line.filtered(lambda x: x.id not in tax_linked_ids and x.product_id.id == int(product_id) and x.product_id.product_tmpl_id.taxable_product_id and x.product_id.product_tmpl_id.taxable_product_id.id == product.taxable_product_id.id)
+            taxable = request.env['product.product'].sudo().search([('product_tmpl_id', '=', product.taxable_product_id.id)], limit=1)
+            for i in prod_lines:
+                order._cart_update(
+                    product_id=int(taxable.id),
+                    set_qty=i.product_uom_qty,
+                    attributes=attributes,
+                    linked_line_id=i.id
+                )
+	
+        return str(order.cart_quantity)
--- custom_product_pricelist/models/product_pricelist.py
+++ custom_product_pricelist/models/product_pricelist.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
 from odoo import api, fields, models
 
+_logger = logging.getLogger(__name__)
 
 class ProdPricelist(models.Model):
     _name = "product.prices"
@@ -35,6 +37,18 @@ class ProdPricelistItems(models.Model):
     attribute6_price = fields.Float('Attribute 6 Price', default=0, digits=(16, 2))
     hardware_discount = fields.Float('Hardware Discount', default=0, digits=(16, 2))
     taxable_amount = fields.Float('Taxable Amount', default=0, digits=(16, 2))
+    att1_price_1 = fields.Float('Attribute 1 Cost /mo', default=0, digits=(16, 2))
+    att1_price_2 = fields.Float('Attribute 1 Custom Price 2', default=0, digits=(16, 2))
+    att2_price_1 = fields.Float('Attribute 2 Cost /mo', default=0, digits=(16, 2))
+    att2_price_2 = fields.Float('Attribute 2 Custom Price 2', default=0, digits=(16, 2))
+    att3_price_1 = fields.Float('Attribute 3 Cost /mo', default=0, digits=(16, 2))
+    att3_price_2 = fields.Float('Attribute 3 Custom Price 2', default=0, digits=(16, 2))
+    att4_price_1 = fields.Float('Attribute 4 Cost /mo', default=0, digits=(16, 2))
+    att4_price_2 = fields.Float('Attribute 4 Custom Price 2', default=0, digits=(16, 2))
+    att5_price_1 = fields.Float('Attribute 5 Cost /mo', default=0, digits=(16, 2))
+    att5_price_2 = fields.Float('Attribute 5 Custom Price 2', default=0, digits=(16, 2))
+    att6_price_1 = fields.Float('Attribute 6 Cost /mo', default=0, digits=(16, 2))
+    att6_price_2 = fields.Float('Attribute 6 Custom Price 2', default=0, digits=(16, 2))
     product_skus = fields.Char("Product Sku's")
 
     @api.model
@@ -46,7 +60,7 @@ class ProdPricelistItems(models.Model):
         
         res = super(ProdPricelistItems, self).create(vals)
         
-        self._add_update_pricelist_items()
+        res._add_update_pricelist_items()
         
         return res
 
@@ -63,21 +77,21 @@ class ProdPricelistItems(models.Model):
 
     def _get_attribute_price(self, att_value_ids):
         if any(atts.id == self.prices_id.attribute1_id.id for atts in att_value_ids):
-            return {'att_id': self.prices_id.attribute1_id.id, 'price': self.attribute1_price}
+            return {'att_id': self.prices_id.attribute1_id.id, 'price': self.attribute1_price, 'price_1': self.att1_price_1, 'price_2': self.att1_price_2}
         elif any(atts.id == self.prices_id.attribute2_id.id for atts in att_value_ids):
-            return {'att_id': self.prices_id.attribute2_id.id, 'price': self.attribute2_price}
+            return {'att_id': self.prices_id.attribute2_id.id, 'price': self.attribute2_price, 'price_1': self.att2_price_1, 'price_2': self.att2_price_2}
         elif any(atts.id == self.prices_id.attribute3_id.id for atts in att_value_ids):
-            return {'att_id': self.prices_id.attribute3_id.id, 'price': self.attribute3_price}
+            return {'att_id': self.prices_id.attribute3_id.id, 'price': self.attribute3_price, 'price_1': self.att3_price_1, 'price_2': self.att3_price_2}
         elif any(atts.id == self.prices_id.attribute4_id.id for atts in att_value_ids):
-            return {'att_id': self.prices_id.attribute4_id.id, 'price': self.attribute4_price}
+            return {'att_id': self.prices_id.attribute4_id.id, 'price': self.attribute4_price, 'price_1': self.att4_price_1, 'price_2': self.att4_price_2}
         elif any(atts.id == self.prices_id.attribute5_id.id for atts in att_value_ids):
-            return {'att_id': self.prices_id.attribute5_id.id, 'price': self.attribute5_price}
+            return {'att_id': self.prices_id.attribute5_id.id, 'price': self.attribute5_price, 'price_1': self.att5_price_1, 'price_2': self.att5_price_2}
         elif any(atts.id == self.prices_id.attribute6_id.id for atts in att_value_ids):
-            return {'att_id': self.prices_id.attribute6_id.id, 'price': self.attribute6_price}
-        #elif any(atts.id == self.prices_id.bib_attribute_id.id for atts in att_value_ids):
-        #    return {'att_id': self.prices_id.bib_attribute_id.id, 'price': self.bib_price_discount}
+            return {'att_id': self.prices_id.attribute6_id.id, 'price': self.attribute6_price, 'price_1': self.att6_price_1, 'price_2': self.att6_price_2}
+        elif any(atts.id == self.prices_id.bib_attribute_id.id for atts in att_value_ids):
+            return {'att_id': self.prices_id.bib_attribute_id.id, 'price': self.bib_price_discount, 'price_1': 0, 'price_2': 0}
         else:
-            return {'att_id': False, 'price': self.retail_price}
+            return {'att_id': False, 'price': self.retail_price, 'price_1': 0, 'price_2': 0}
 
     def _check_and_set_product(self, product_id, skus):
         if product_id:
@@ -89,13 +103,14 @@ class ProdPricelistItems(models.Model):
                 return prod_ids[0].id
                 
     def _striplist(self, list):
-        return ([x.strip() for x in list])
+        return ([x.strip() for x in list if x.strip()])
     
     def _add_update_pricelist_items(self):
         Pricelist = self.env['product.pricelist']
         Product = self.env['product.product']
         ProductPrice = self.env['product.attribute.price']
-        plList = Pricelist.search([('id', 'in', self.prices_id.pricelist_id.ids)])
+        plList = Pricelist.search([('id', 'in', self.prices_id.pricelist_id.ids)])  
+        #self.prices_id.pricelist_id
         ppList = Product.search([('product_tmpl_id', '=', self.product_id.id)])
         
         if not self.product_id:
@@ -107,44 +122,97 @@ class ProdPricelistItems(models.Model):
             val['taxable_amount'] = self.taxable_amount
         self.product_id.write(val)
         
-        #Add or update the Bring it Back option
-        bibId = self.prices_id.bib_attribute_id.id
-        bibPrice = self.bib_price_discount * -1
-        pap = ProductPrice.search(['&', ('value_id', '=', bibId), ('product_tmpl_id', '=', self.product_id.id)])
-        if pap:
-            pap.write({'price_extra': bibPrice })
-        else:
-            ProductPrice.create({'product_tmpl_id': self.product_id.id, 'value_id': bibId, 'price_extra': bibPrice})
+        
+        #Loop through the available Pricelists
+        for pp in ppList:
+            for pl in plList:
+                #Add or update the Bring it Back option
+                bibId = self.prices_id.bib_attribute_id.id
+                if any(atts.id == bibId for atts in pp.attribute_value_ids):
+                    bibPrice = self.bib_price_discount * -1
+                else:
+                    bibPrice = 0
+                item = pl.item_ids.filtered(lambda x: x.product_id.id == pp.id)
+                #(['&', ('product_id', '=', pp.id), ('pricelist_id', '=', pp.id)])
+                fixedPrice = self._get_attribute_price(pp.attribute_value_ids)
+                finalFixedPrice = (0 if fixedPrice['price'] + bibPrice < 0 else fixedPrice['price'] + bibPrice)
+                taxable = self.taxable_amount if self.taxable_amount else finalFixedPrice
+                per_month = fixedPrice['price_1'] + round((0 if fixedPrice['price'] + bibPrice >= 0 else fixedPrice['price'] + bibPrice)/24, 2)
+                final_per_month = (0 if per_month < 0 else per_month)
+                if item:
+                    item.write({'fixed_price': finalFixedPrice, 'price_1': final_per_month, 'price_2': fixedPrice['price_2']})
+                
+                else:
+                    self.env['product.pricelist.item'].create({
+                        'fixed_price': finalFixedPrice,
+                        'product_id': pp.id,
+                        'applied_on': '0_product_variant',
+                        'compute_price': 'fixed',
+                        'pricelist_id': pl.id,
+                        'price_1': final_per_month,
+                        'price_2': fixedPrice['price_2']
+                    })
+                    
+                if not pp.product_tmpl_id.taxable_product_id:
+                    new_template = pp.product_tmpl_id.create({
+                        'name': 'Taxable - ' + pp.product_tmpl_id.name,
+                        'active': True,
+                        'image': pp.product_tmpl_id.image,
+                        'image_medium': pp.product_tmpl_id.image_medium,
+                        'sale_ok': True,
+                        'website_published': False
+                    })
+                    pp.product_tmpl_id.write({'taxable_product_id': new_template.id})
+                else:
+                    new_template = pp.product_tmpl_id.taxable_product_id
+                    
+                tmpl_exists = pl.item_ids.filtered(lambda x: x.product_tmpl_id.id == new_template.id)
+                if tmpl_exists:
+                    if tmpl_exists[0].fixed_price != taxable:
+                        tmpl_exists[0].write({'fixed_price': taxable})
+                else:
+                    pl.item_ids.create({
+                        'fixed_price': taxable,
+                        'product_tmpl_id': new_template.id,
+                        'applied_on': '1_product',
+                        'compute_price': 'fixed',
+                        'pricelist_id': pl.id
+                    })
+        #pap = ProductPrice.search(['&', ('value_id', '=', bibId), ('product_tmpl_id', '=', self.product_id.id)])
+        #if pap:
+        #    pap.write({'price_extra': bibPrice })
+        #else:
+        #    ProductPrice.create({'product_tmpl_id': self.product_id.id, 'value_id': bibId, 'price_extra': bibPrice})
             
         #Loop through the product.product items to update the product.attribute.prices and add to the Pricelist.
-        for pp in ppList:
-            attPrice = self._get_attribute_price(pp.attribute_value_ids)
-            price = attPrice['price'] - self.retail_price
-            pap = ProductPrice.search(['&', ('value_id', '=', attPrice['att_id']), ('product_tmpl_id', '=', pp.product_tmpl_id.id)])
-            if pap:
-                pap.write({'price_extra': price})
-            else:
-                ProductPrice.create({'product_tmpl_id': pp.product_tmpl_id.id, 'value_id': attPrice['att_id'], 'price_extra': price})
+        #for pp in ppList:
+        #    attPrice = self._get_attribute_price(pp.attribute_value_ids)
+        #    price = attPrice['price'] - self.retail_price
+        #    pap = ProductPrice.search(['&', ('value_id', '=', attPrice['att_id']), ('product_tmpl_id', '=', pp.product_tmpl_id.id)])
+        #    if pap:
+        #        pap.write({'price_extra': price})
+        #    else:
+        #        ProductPrice.create({'product_tmpl_id': pp.product_tmpl_id.id, 'value_id': attPrice['att_id'], 'price_extra': price})
             
             #Add, Update, or remove a Pricelist item if the price is negative
-            if attPrice['att_id'] != bibId and any(attId.id == bibId for attId in pp.attribute_value_ids):
-                for pl in plList:
-                    item = pl.item_ids.search(['&', ('product_id', '=', pp.id), ('pricelist_id', '=', pl.id)])
-                    bibPrice = attPrice['price'] - self.bib_price_discount
+        #    if attPrice['att_id'] != bibId and any(attId.id == bibId for attId in pp.attribute_value_ids):
+        #        for pl in plList:
+        #            item = pl.item_ids.search(['&', ('product_id', '=', pp.id), ('pricelist_id', '=', pl.id)])
+        #            bibPrice = attPrice['price'] - self.bib_price_discount
 
-                    if item:
-                        if 0 > bibPrice != item.fixed_price:
-                            item.write({'fixed_price': 0})
-                        elif bibPrice >= 0:
-                            item.unlink()
-                    elif bibPrice <= 0:
-                        self.env['product.pricelist.item'].create({
-                            'fixed_price': 0,
-                            'product_id': pp.id,
-                            'applied_on': '0_product_variant',
-                            'compute_price': 'fixed',
-                            'pricelist_id': pl.id
-                        })
+        #            if item:
+        #                if 0 > bibPrice != item.fixed_price:
+        #                    item.write({'fixed_price': 0})
+        #                elif bibPrice >= 0:
+        #                    item.unlink()
+        #            elif bibPrice <= 0:
+        #                self.env['product.pricelist.item'].create({
+        #                    'fixed_price': 0,
+        #                    'product_id': pp.id,
+        #                    'applied_on': '0_product_variant',
+        #                    'compute_price': 'fixed',
+        #                    'pricelist_id': pl.id
+        #                })
 
 
 class ProductTemplate(models.Model):
@@ -153,3 +221,12 @@ class ProductTemplate(models.Model):
     hardware_discount = fields.Float('Hardware Discount', default=0, digits=(16, 2))
     taxable_amount = fields.Float('Taxable Amount', default=0, digits=(16, 2))
     product_skus = fields.Char("Product Sku's")
+    taxable_product_id = fields.Many2one('product.template', 'Taxable Product')
+    
+    
+class product_pricelist(models.Model):
+    _inherit = "product.pricelist.item"
+    
+    price_1 = fields.Float(string='Custom Price 1')
+    price_2 = fields.Float(string='Custom Price 2')
+    taxable_amount = fields.Float('Taxable Amount', default=0, digits=(16, 2))
