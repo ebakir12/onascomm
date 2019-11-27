11e58d8b970c2a10bbeaa401a571f5fa0e9b1b78
Eihab Bakir <eihab.bakir@connectedgateway.com>
2019-11-27 05:06:49 -0600

    Add files via upload

---
+++ manage_products/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Manage Products',
+    'category': 'Hidden',
+    'version': '1.0',
+    'description':
+        """
+Manage new and existing product prices from a central repository
+========================
+
+This module will provide access to the products to allow updates from a central repository
+        """,
+    'depends': ['website_sale'],
+    'auto_install': False,
+    'data': [
+        
+    ]
+}
